0078c23648966085a9351fae340398d62767f281
Javad Ibrahimli <cenabibrahimov@gmail.com>
2023-01-12 01:03:51 +0300

    Create Correlation-and-covariance.r

---
+++ codes/Correlation-and-covariance.r
@@ -0,0 +1,9 @@
+# Define a vector of values for two random variables
+x <- c(1, 2, 3)
+y <- c(4, 5, 6)
+
+# Calculate the correlation between the two random variables
+cor(x, y)
+
+# Calculate the covariance between the two random variables
+cov(x, y)
