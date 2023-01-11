9053b57170f50487a0934af16c1151ad36fce764
Javad Ibrahimli <cenabibrahimov@gmail.com>
2023-01-12 00:51:14 +0300

    Create median.r

---
+++ codes/median.r
@@ -0,0 +1,5 @@
+numbers <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+median(numbers)
+
+#The median() function in R takes a vector of numbers as an argument, and it returns the median value of that vector.
+#In the above example, I've created a variable numbers which contains an set of numbers, You can replace it with any set of numbers and run the function median(numbers) and it will return the median of the numbers you provided in your set.
