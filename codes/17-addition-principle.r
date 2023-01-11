c3dd5e98a6a9294a0ae96f379c9e3a94578ada71
Javad Ibrahimli <cenabibrahimov@gmail.com>
2023-01-12 01:14:56 +0300

    Create 17-addition-principle.r

---
+++ codes/17-addition-principle.r
@@ -0,0 +1,5 @@
+# Using the addition principle to calculate the number of outcomes in mutually exclusive events
+event1_outcomes <- 5
+event2_outcomes <- 6
+event3_outcomes <- 4
+event1_outcomes + event2_outcomes + event3_outcomes
