f583c6555e165409b193df72d6d76861dfae357a
Javad Ibrahimli <cenabibrahimov@gmail.com>
2023-01-12 00:59:45 +0300

    Create Generating-random-variables.r

---
+++ codes/Generating-random-variables.r
@@ -0,0 +1,8 @@
+# Generate a random number between 0 and 1
+runif(1)
+
+# Generate a random sample of size 10 from a normal distribution with mean 0 and standard deviation 1
+rnorm(10, mean = 0, sd = 1)
+
+# Generate a random sample of size 5 from a binomial distribution with n = 10 and p = 0.5
+rbinom(5, size = 10, prob = 0.5)
