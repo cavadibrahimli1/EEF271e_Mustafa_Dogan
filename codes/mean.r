753d9628ec5c654ca199000ff1e13060cf0fd96e
Javad Ibrahimli <cenabibrahimov@gmail.com>
2023-01-12 00:46:53 +0300

    Create mean.r

---
+++ codes/mean.r
@@ -0,0 +1,21 @@
+# Create a vector of numbers
+numbers <- c(1, 2, 3, 4, 5)
+
+# Calculate the mean of the numbers
+numbers_mean <- mean(numbers)
+
+# Print the result
+print(numbers_mean)
+
+
+#You can change the numbers in the numbers vector to calculate the mean of any set of numbers. The mean() function is used to calculate the mean. The result will be printed on the console using the print() function.
+#You can also use the sum() function and length() function together to find the mean of numbers.
+
+numbers <- c(1, 2, 3, 4, 5)
+numbers_mean <- sum(numbers)/length(numbers)
+print(numbers_mean)
+
+#Both codes will give the same output, mean of the numbers passed in the vector.
+
+
+
